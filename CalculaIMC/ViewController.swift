91de66e9f142ef7d093357e7e0579d83c9fd613e
Guilherme Ferreira <360guilherme@gmail.com>
2018-07-24 10:46:12 -0300

    pegando valores digitados pelo usuário e calculando o IMC

--- CalculaIMC/ViewController.swift
+++ CalculaIMC/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var exibeReultado: UILabel!
+    @IBOutlet weak var digitaPeso: UITextField!
+    @IBOutlet weak var digitaAltura: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +23,28 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //metodo chamado quando um toque ocorre na tela
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        digitaPeso.resignFirstResponder()
+        digitaAltura.resignFirstResponder()
+    }
+    
+    //TRATA O EVENTO DE BOTÃO PARA CALCULAR O IMC
+    @IBAction func handleIMC(_ sender: UIButton) {
+        if  let peso = Float(digitaPeso.text!),
+            let altura = Float(digitaAltura.text!){
+            let IMC = peso / (altura * altura)
+            
+            if(IMC <= 16){
+                exibeReultado.text = "MAGRINHO"
+            }else if(IMC <= 25){
+                exibeReultado.text = "NORMAL"
+            }else{
+                exibeReultado.text = "GORDINHO"
+            }
+        }
+        
+    }
 }
 
